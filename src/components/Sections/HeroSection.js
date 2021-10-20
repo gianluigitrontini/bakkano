283e6c3d1b3eeb36e5ff544ac17217f247d99ac8
Gianluigi Trontini <gianluigitrontini@gmail.com>
2021-10-20 17:20:46 +0200

    Fix fixed image overflowing body

--- src/components/Sections/HeroSection.js
+++ src/components/Sections/HeroSection.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Fade } from 'react-awesome-reveal';
-import { useEffect } from 'react/cjs/react.development';
 // import { Link } from 'react-scroll';
 // import FirmaChef from '../../assets/images/firma-chef.png';
 import pinsaConSalame from '../../assets/images/foto-pinsa-con-salame.png';
@@ -37,34 +36,34 @@ function HeroSection() {
 
   const Carousel = ({ carouselData }) => {
     useEffect(() => {
-      const interval = setInterval(() => {
-        if (slideNumber == 3) {
-          setSlideNumber(1);
-        } else {
-          setSlideNumber((slideNumber) => slideNumber + 1);
-        }
-      }, 5000);
-      return () => clearInterval(interval);
+      // const interval = setInterval(() => {
+      //   if (slideNumber == 3) {
+      //     setSlideNumber(1);
+      //   } else {
+      //     setSlideNumber((slideNumber) => slideNumber + 1);
+      //   }
+      // }, 5000);
+      // return () => clearInterval(interval);
     }, []);
 
     return carouselData.map((item) => {
       return (
         <div
           key={`carousel-${item.slideId}`}
-          className={`${
-            slideNumber.toString() === item.slideId
-              ? 'opacity-100'
-              : 'opacity-0 h-0'
+          className={`z-0 overflow-hidden  ${
+            slideNumber.toString() === item.slideId ? 'h-screen' : 'hidden'
           }`}>
           <img
             alt='Una pinsa appena sfornata e tagliata'
             src={item.image}
-            className='animate-zoom-slowly fixed opacity-100 h-screen top-0 left-0 right-0 w-full transition-opacity object-cover bg-no-repeat'
+            className={` ${
+              slideNumber.toString() === item.slideId ? 'h-screen' : 'hidden'
+            } animate-zoom-slowly z-0 h-full fixed opacity-100  top-0 left-0 right-0 bottom-0 w-full transition-opacity object-cover`}
           />
           <div className='fixed bg-black opacity-50 top-0 right-0 left-0 bottom-0'></div>
 
           <div className='h-screen container mx-auto flex items-center w-full'>
-            <div className=' flex flex-col w-fulllg:w-2/3 font-semibold text-white uppercase '>
+            <div className='flex flex-col w-full lg:w-2/3 font-semibold text-white uppercase '>
               <Fade>
                 <div className='flex flex-col'>
                   <span className='block text-5xl lg:text-8xl'>
